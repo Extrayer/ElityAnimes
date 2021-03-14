41f245e01652be403010608962d727b3533d147c
Extrayer <64549344+Extrayer@users.noreply.github.com>
2021-03-14 14:09:57 +0100

    +

--- animes/haikyu/script.py
+++ animes/haikyu/script.py
@@ -1,21 +1,32 @@
 import os
 
+depart = int(input("Episode de départ ? : "))
+number = int(input("Episode de fin ? : "))
+tags = str(input("Tags ? : "))
+title = str(input("Title ? : "))
+saison = str(input("Saison ? : "))
+cc = str(input("cc ? : "))
 
-for i in range(18,22):
 
-    myFile = open(str(i)+".md", "w+")
-    myFile.write(f"""---
+for i in range(depart,number+1):
+    if len(str(i)) == 1:
+        i = '0' + str(i)
+    
+
+    File = open(str(i)+".md", "w+")
+    File.write(f'''---
 layout: lecteur.njk
-tags: jjk
+tags : {tags}
+
+title : {title}
+episode : {i}
+saison : {saison}
+iframe :
+cc :  {cc}
+    
+---''')
 
 
-title: Jujutsu Kaisen
-episode: {i}
-saison: 1
-iframe: 
-cc: VostFr
 
----
-    """)
 
 
